f4a82e6bd116a8049f130d22805609049dd7bc83
Gowreesh A.M <manekshaw70@gmail.com>
2023-03-17 09:45:30 +0530

    created mongoose schema for contacts

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express'); // Import express
+
 const connectDB = require('./config/dbConnection');
-// const connectDB = require('mongoose')
-// connectDB.connect('mongodb+srv://Gowreesh:V%40tt%40r76@gowreeshcluster.momqdv3.mongodb.net/mycontacts-backend')
-// connectDB.set('strictQuery', true);
+
 const dotenv = require('dotenv').config() // Import dotenv
+
 connectDB(); // Connect to database
+
 const app = express(); // Create express app
 
 const errorHandler = require('./middleware/errorHandler') // Import error handler
