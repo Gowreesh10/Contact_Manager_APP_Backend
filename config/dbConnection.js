95d5ce27c7dff5cf8ff866b5920ffc841e8184f5
GOWREESH_A_M <68791658+Gowreesh10@users.noreply.github.com>
2024-02-23 16:57:36 +0530

    Update dbConnection.js

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try{
-        const connect = await mongoose.connect(process.env.CONNECTION_STRING);
+        const connect = await mongoose.connect('mongodb+srv://Gowreesh:Admin1234@gowreeshcluster.bg0gwkp.mongodb.net/?retryWrites=true&w=majority&appName=Gowreeshcluster');
         console.log('Database connected :',connect.connection.host,connect.connection.name)
     }catch(err){
         console.log(err);
